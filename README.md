6ca71870e6c81bd0841725e0547cd7af6ef9e99c
= <matti.pastell@helsinki.fi>
2016-02-05 18:58:00 +0200

    Fix typos

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # Python for Agricultural Engineers
 
-This Python package is used in my (not quite finished) book  " Measurements and Data Analysis for Agricultural Engineers using Python". 
+This Python package is used in my (not quite finished) book  " Measurements and Data Analysis for Agricultural Engineers Using Python". 
 
 You can read the current version book online at: http://pyageng.mpastell.com/book
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='pyageng',
 Python for Agricultural Engineers
 ---------------------------------
 
-This Python package is used in my (not quite finished) book  " Measurements and Data Analysis for Agricultural Engineers using Python".
+This Python package is used in my (not quite finished) book  " Measurements and Data Analysis for Agricultural Engineers Using Python".
 
 You can read the current version book online at: `<http://pyageng.mpastell.com/book>`_
 """,
